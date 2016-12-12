d943c8c610ccdd31be966e10a3d159baac9f277d
Prajesh Ananthan <prajesh.ananthan@bertelsmann.de>
2016-12-12 19:55:13 +0800

    Initial commit for button with image test

---
+++ test/test_button.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class View(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        self.image = tk.PhotoImage(file="images/download-icon.png")
+        b = tk.Button(self, text="Download", image=self.image, compound="left")
+        b.pack(side="top")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    view = View(root)
+    view.pack(side="top", fill="both", expand=False)
+    root.mainloop()
