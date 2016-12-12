f3bbb794cbfeed2c45fa2d8b1567ea889edea2c1
Prajesh Ananthan <prajesh.ananthan@bertelsmann.de>
2016-12-13 07:21:27 +0800

    Changed directory structure

--- test/test_button.py
+++ test/test_button.py
@@ -3,7 +3,7 @@ import tkinter as tk
 class View(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
-        self.image = tk.PhotoImage(file="images/download-icon.png")
+        self.image = tk.PhotoImage(file="resources/images/download-icon.png")
         b = tk.Button(self, text="Download", image=self.image, compound="left")
         b.pack(side="top")
 
